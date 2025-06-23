3bd1d2f4a90546564ab345c012fd931bfeae0a42
JoseMontesU <jmontesu@bcds.net>
2025-06-22 20:24:00 -0500

    arreglando ms-correos

--- src/services/envio-correos/envio-correos.module.ts
+++ src/services/envio-correos/envio-correos.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EnvioCorreosService } from './service/envio-correos.service';
 import { EnvioCorreosController } from './controller/envio-correos.controller';
+import { CorreoRepository } from './repository/correo.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Correos } from './entity/correo.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Correos])],
   controllers: [EnvioCorreosController],
-  providers: [EnvioCorreosService],
+  providers: [EnvioCorreosService, CorreoRepository],
 })
 export class EnvioCorreosModule {}
